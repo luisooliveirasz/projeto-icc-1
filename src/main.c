994b1fe13b298b146a2e84c3d7bc46ccd1fc1465
c-pignatellio <cl203229@g.unicamp.br>
2026-06-11 09:13:53 -0300

    parametros e loop de comando

--- src/main.c
+++ src/main.c
@@ -21,15 +21,74 @@ Finalizar o dia   - FE
 */
 
 // Assinaturas das funções
-void inserir_produto();
-void aumentar_estoque();
-void modificar_preco();
-void vender();
+void inserir_produto(int *nprodutos,char nome_produto[100],int qntd_produto,float preco_produto);
+void aumentar_estoque(int codigo_produto, int qntd_aumentar);
+void modificar_preco(int codigo_produto, float novo_preco);
+void vender(int codigo_produto,int *preco_total);
 void consultar_estoque();
 void consultar_saldo();
 void finalizar_dia();
 
 int main(void)
+{
+    int nProdutos =0;
+    char comando[2];
+    scanf(" %s", comando);
+
+    while(comando[0] != 'F' || comando[1] != 'E')
+    {
+        if(comando == 'IP')
+        {
+            char nome_produto[100];
+            int qntd_produto;
+            float preco_produto;
+            scanf(" %s %d %f", nome_produto, &qntd_produto, &preco_produto);
+            inserir_produto(&nProdutos, nome_produto, qntd_produto, preco_produto);
+        }
+        else if(comando == 'AE')
+        {
+            int codigo_produto;
+            int qntd_aumentar;
+            scanf(" %d %d", &codigo_produto, &qntd_aumentar);
+            aumentar_estoque(codigo_produto, qntd_aumentar);
+        }
+        else if(comando == 'MP')
+        {
+            int codigo_produto;
+            float novo_preco;
+            scanf(" %d %f", &codigo_produto, &novo_preco);
+            modificar_preco(codigo_produto, novo_preco);
+        }
+        else if(comando == 'VE')
+        {
+            int codigo_produto;
+            int preco_total=0;
+            scanf(" %d", &codigo_produto);
+            while(codigo_produto != -1)
+            {
+                vender(codigo_produto, &preco_total);
+                scanf(" %d", &codigo_produto);
+            }
+            printf("Total: %d\n", preco_total);
+            
+        }
+        else if(comando == 'CE')
+        {
+            consultar_estoque();
+        }
+        else if(comando == 'CS')
+        {
+            consultar_saldo();
+        }
+        scanf(" %s", comando);
+    }
+    
+    finalizar_dia();
+    
+
+}
+
+void inserir_produto(int *nprodutos, char nome_produto[100],int qntd_produto,float preco_produto)
 {
 
 }
